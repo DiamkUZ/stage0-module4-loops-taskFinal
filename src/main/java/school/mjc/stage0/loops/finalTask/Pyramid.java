deec2ea55a06c37951c372e2bcf8e8833c2170dd
DiamkUZ <diamkms@yandex.ru>
2023-04-14 14:33:03 +0300

    all done

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,22 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i<=cathetusLength; i++) {
+            for (int j = cathetusLength; j >= 1; j--) {
+                if (j > i) {
+                    System.out.print(" ");
+                    }
+                else System.out.print(j);
+            }
+            for (int k=2;k <= i; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
 
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(5);
     }
 }
